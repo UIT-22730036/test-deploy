74b1f63ee4a87466dbd84df2323e4fd6748f81ce
khang.huynh.tpv <khang.huynh.tpv@one-line.com>
2025-12-17 16:25:45 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HttpService } from '@nestjs/axios';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly httpService: HttpService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getUsers() {
+    return this.httpService.get(
+      'https://4ln2.developer.orkescloud.com/users/get/users/get',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      },
+    );
   }
 }
