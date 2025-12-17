517acbbbd42afc089d63399d7477afbc60abbba9
khang.huynh.tpv <khang.huynh.tpv@one-line.com>
2025-12-17 17:21:13 +0700

    init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { HttpService } from '@nestjs/axios';
 import * as dotenv from 'dotenv';
@@ -24,6 +24,21 @@ export class AppController {
       {
         headers: {
           Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+      },
+    );
+  }
+
+  @Post('/users/start')
+  start() {
+    return this.httpService.post(
+      'https://4ln2.developer.orkescloud.com/users/start',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_KEY}`,
+        },
+        body: {
+          until: '2025-12-17 17:30 GMT+07:00',
         },
       },
     );
